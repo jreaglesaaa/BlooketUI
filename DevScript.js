132129032404451de46d4643c91093d72ece5001
zastix <69748767+ZasticBradyn@users.noreply.github.com>
2022-02-27 20:55:47 -0600

    Create DevScript.js

---
+++ DevScript.js
@@ -0,0 +1,1 @@
+// hello this file just will have a version of the ui that is in early acces, therefore i would reccomend using the normal Script.js (the devscript isnt coded yet so their is nothing in this file)
